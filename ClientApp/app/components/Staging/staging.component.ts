111b18152d3c5549bb3722be551ad6bf0c5b5ecb
Unknown <simon.palenchar3@gmail.com>
2017-11-28 11:39:07 -0500

    add footnote and add definition added

--- ClientApp/app/components/Staging/staging.component.ts
+++ ClientApp/app/components/Staging/staging.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ServiceMaster } from '../Services/ServiceMaster';
 import { Http } from '@angular/http';
-import { Metric, Source } from '../Models/Models'; 
+import { Metric, Source, Meta } from '../Models/Models'; 
 
 @Component({
     selector: 'staging',
@@ -15,11 +15,17 @@ export class StagingComponent {
     metricEdit: Metric;
     sources: Source[];
     edited: boolean;
+    meta: Meta;
+    me: Meta;
+    def: boolean;
+    foot: boolean;
     constructor(service: ServiceMaster, http: Http) {
         this.svc = service;
         this.http = http;
         this.Search = false;
         this.edited = false;
+        this.foot = false;
+        this.def = false;
     }
 
     ngOnInit() {
@@ -44,6 +50,21 @@ export class StagingComponent {
             this.metricEdit = await response
 
         });
+        if (this.metricEdit.meta.length > 0)
+        {
+            for (var i = 0; i < this.metricEdit.meta.length; i++)
+            {
+                this.me = this.metricEdit.meta[i];
+                if (this.me.type.charAt(0) == 'D')
+                {
+                    this.def = true;
+                }
+                else if (this.me.type.charAt(0) == 'F')
+                {
+                    this.foot = true;
+                }
+            }
+        }
         await this.svc.getStagingSource(m.id).then(async response => {
             this.sources = await response
         });
@@ -61,5 +82,20 @@ export class StagingComponent {
         this.Search = false;
         console.log("editing" + this.metricEdit.id.toString());
         this.SearchID = "";
+    }
+    addDef() {
+        this.meta = new Meta();
+        this.meta.type = "Definition",
+        this.meta.data = ""
+        this.metricEdit.meta[this.metricEdit.meta.length] = this.meta;
+        this.def = true;
+    }
+
+    addFoot() {
+        this.meta = new Meta();
+        this.meta.type = "Footnotes",
+        this.meta.data = ""
+        this.metricEdit.meta[this.metricEdit.meta.length] = this.meta;
+        this.foot = true;
     }
 }
